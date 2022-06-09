6d053da8f9d8b859472140e216e2a9641676c0cb
NZ-WEB <stereo00@yandex.ru>
2022-06-09 13:07:26 +0300

    feat(history): add main destinations in nirs page
    Added UI component TheNirsDestinations

---
+++ src/components/TheNirsDestinations.vue
@@ -0,0 +1,23 @@
+<template>
+  <h4 class="text-h5 text-center q-py-md text-primary text-bold">Основные направления НИОКР</h4>
+  <ul class="column justify-center">
+    <li
+      class="text-center"
+      v-for="destination in destinations"
+      :key="destinations.indexOf(destination)"
+    >
+      <q-item-label class="text-body1 text-weight-medium q-py-sm text-primary">
+        {{ destination }}
+      </q-item-label>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+  destinations: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
