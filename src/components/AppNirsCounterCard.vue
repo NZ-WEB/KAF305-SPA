2e78b3d67b491075f7ae1e9272e4d8ad6cb81ad4
NZ-WEB <stereo00@yandex.ru>
2022-06-09 14:10:55 +0300

    feat(history): add achievements count
    Added UI component TheNirsCounter in which the universal achievements card component was created

---
+++ src/components/AppNirsCounterCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-card
+    class="q-pa-sm clear-padding-sm shadow-0 text-primary custom-card"
+    style="background: #edf4fa"
+  >
+    <q-card-section class="row no-wrap q-gutter-sm">
+      <span class="custom-card__main-number">{{mainNumber}}</span>
+        <p class="text-body1">
+          {{ title }}
+        </p>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+const props = defineProps({
+  mainNumber: {
+    type: Number,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.custom-card {
+
+  &__main-number {
+    font-size: 100px;
+    line-height: 100px;
+  }
+}
+</style>
---
+++ src/components/TheNirsCounter.vue
@@ -0,0 +1,24 @@
+<template>
+  <h4 class="text-h5 text-center q-py-md text-primary text-bold">В период с 2015-2021 гг. кафедрой выполнен большой
+    объем НИР
+  </h4>
+
+  <div class="row justify-center q-gutter-md q-gutter-md">
+    <div class="col-3" v-for="nirsCounter in nirsCounters" :key="nirsCounter.mainNumber">
+      <app-nirs-counter-card  :main-number="nirsCounter.mainNumber" :title="nirsCounter.title" />
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+import AppNirsCounterCard from "components/AppNirsCounterCard";
+
+const props = defineProps({
+  nirsCounters: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
